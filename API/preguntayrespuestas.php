d6373d7077f0362802cd289556f22c7fc1d17293
Luismp96 <119935630+Luismp96@users.noreply.github.com>
2024-01-20 17:47:15 +0100

    Parte 2 Proyecto
    Se añade Parte 2 Proyecto

---
+++ API/preguntayrespuestas.php
@@ -0,0 +1,39 @@
+<?php
+
+    include "../config.php";
+
+    $peticion = "
+    SELECT * FROM preguntas 
+    WHERE identificador = ".$_GET['id']."
+    ;
+    ";
+    $resultado = mysqli_query($conexion,$peticion);
+    $datos = [];
+        
+    while($fila = mysqli_fetch_assoc($resultado)){
+        $datos[] = $fila;
+
+    }
+
+    $contenido['pregunta'] = $datos;
+
+    $peticion = "
+    SELECT * FROM respuestas 
+    WHERE idpregunta = ".$_GET['id']."
+    ;
+    ";
+
+    $resultado = mysqli_query($conexion,$peticion);
+    $respuestas = [];
+
+    while($fila = mysqli_fetch_assoc($resultado)){
+        $respuestas[] = $fila;
+
+    }
+
+    $contenido['respuestas'] = $respuestas;
+
+    $json = json_encode($contenido,JSON_PRETTY_PRINT);
+    echo $json;
+    
+?>
--- cliente/js/preguntas.js
+++ cliente/js/preguntas.js
@@ -1,23 +1,136 @@
 
 //CARGA INICIAL PREGUNTAS AL ABRIR VENTANA
 window.onload = function(){
-    fetch("../../proyecto/API/preguntas.php")
+    fetch("../../proyectocopia/API/preguntas.php")
     .then(function(response){
         return response.json()
     })
+
     //UTILIZANDO TEMPLATE
     .then(function(datos){
+
+        console.log(datos);
 
         let plantilla = document.getElementById('plantillapregunta');
         let seccion = document.querySelector('section');
 
         for(let i=0;i<datos.length;i++){
             let importado = document.importNode(plantilla.content,true);
+            //SE PASADA IDENTIFICADOR PARA PONER COMO NAME A CADA UNA DE LAS ETIQUETAS HIJAS
+            importado.querySelector('article').setAttribute('name',datos[i].identificador);
             importado.querySelector('h3').textContent = datos[i].titulo;
             importado.querySelector('time').textContent = datos[i].fecha;
             importado.querySelector('p').textContent = datos[i].texto;
+
+            importado.querySelector('article').onclick = function(){
+                let identificador = this.getAttribute('name');
+
+                console.log("Has hecho click en la pregunta con ID: " + identificador);
+                console.log(plantilla.content);
+                cargaArticuloSeleccionado(identificador);
+            }
             seccion.appendChild(importado);
         }
     })
 
+    document.getElementById('iniciarsesion').onclick = function(){
+        console.log("Vamos a Iniciar Sesion");
+        document.getElementById('modal').style.display = "block";
+        document.getElementById('contenedormodal').innerHTML = "";
+
+        let plantilla = document.getElementById('iniciosesion');
+        let importado = document.importNode(plantilla.content,true);
+        document.getElementById('contenedormodal').appendChild(importado);
+
+        document.getElementById('enviainiciosesion').onclick = function(){
+            console.log("Iniciamos Sesion");
+            let nombre = document.getElementById('usuario').value;
+            let contrasena = document.getElementById('contrasena').value;
+
+            console.log(nombre);
+            console.log(contrasena);
+
+            fetch("../../proyectocopia/API/login.php?usuario="+nombre+"&contrasena="+contrasena)
+            .then(function(response){
+                    return response.json()
+            })
+            .then(function(datos){
+
+                console.log(datos);
+
+                if(datos.llave == 'si'){
+                    document.getElementById('modal').style.display = "none";
+                    document.cookie = "usuario=" + nombre + ";";
+                }
+
+                window.location = window.location;
+            })
+
+           
+        }
+    }
+
+    
+
+}
+
+function cargaArticuloSeleccionado(identificador){
+
+    document.querySelector('section').innerHTML = "";
+
+    fetch("../../proyectocopia/API/preguntayrespuestas.php?id="+identificador)
+    .then(function(response){
+        return response.json()
+    })
+
+    //UTILIZANDO TEMPLATE
+    .then(function(datos){
+
+        console.log(datos);
+        //PREGUNTAS
+        let plantilla = document.getElementById('plantillapregunta');
+        let seccion = document.querySelector('section');
+        let pregunta = datos.pregunta[0];
+        let importado = document.importNode(plantilla.content,true);
+        //SE PASADA IDENTIFICADOR PARA PONER COMO NAME A CADA UNA DE LAS ETIQUETAS HIJAS
+        importado.querySelector('article').setAttribute('name',pregunta.identificador);
+        importado.querySelector('h3').textContent = pregunta.titulo;
+        importado.querySelector('time').textContent = pregunta.fecha;
+        importado.querySelector('p').textContent = pregunta.texto;
+
+        seccion.appendChild(importado);
+
+        let cabecerarespuestas = document.getElementById('cabecerarespuestas');
+        let importado1 = document.importNode(cabecerarespuestas.content,true);
+        importado1.querySelector('p').textContent = "Respuestas Pregunta";
+        seccion.appendChild(importado1);
+
+        //RESPUESTA
+        let plantilla1 = document.getElementById('plantillarespuesta');
+        let respuestas = datos.respuestas;
+
+        console.log(respuestas);
+        let contador=0;
+
+        for(let i=0;respuestas.length;i++){
+
+            let importado = document.importNode(plantilla1.content,true);
+            //SE PASADA IDENTIFICADOR PARA PONER COMO NAME A CADA UNA DE LAS ETIQUETAS HIJAS
+            importado.querySelector('article').setAttribute('name',respuestas[i].identificador);
+            importado.querySelector('time').textContent = respuestas[i].fecha;
+            importado.querySelector('p').textContent = respuestas[i].texto;
+            seccion.appendChild(importado);
+            contador++;
+        }
+        console.log(contador);
+
+        if(contador == 0){
+            
+            let plantilla2 = document.getElementById('plantillasinresultado');
+            let importado2 = document.importNode(plantilla2.content,true);
+            importado2.querySelector('p').textContent = "No hay respuestas a esta pregunta";
+            seccion.appendChild(importado2);
+        }
+
+    })
 }
